3acedcc5190ebed3f65e500419e1831f0dc32bf4
rahma408 <61933946+rahma408@users.noreply.github.com>
2020-12-11 15:27:33 +0700

    Add files via upload

---
+++ CouponWithOCP/Coupon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouponWithOCP
+{
+    abstract class Coupon
+    {
+        public abstract double calculate(double originPrice);
+
+    }
+}
---
+++ CouponWithOCP/CouponwithPercentage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouponWithOCP
+{
+    class CouponwithPercentage : Coupon
+    {
+        public double discPercentage = 0;
+
+        public CouponwithPercentage(double discPcentage)
+        {
+            this.discPercentage = discPercentage;
+        }
+
+        public override double calculate(double originPrice)
+        {
+            return (100 - discPercentage) * originPrice / 100;
+        }
+    }
+}
