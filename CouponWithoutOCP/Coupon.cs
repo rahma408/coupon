d4d4d8dd769c94dfbf3601afc4983083847eeecf
rahma408 <61933946+rahma408@users.noreply.github.com>
2020-12-11 15:31:58 +0700

    Add files via upload

---
+++ CouponWithoutOCP/Coupon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoupunWithoutOCP
+{
+    class Coupon
+    {
+        public double discNominal = 0;
+        public double discPercentage =0;
+
+        public double priceNeet(double originPrice)
+        {
+            double net = 0;
+            if(discNominal==0 && discPercentage > 0)
+            {
+                net = (100 - discPercentage) * originPrice / 100;
+            }
+           else if (discNominal > 0 && discPercentage == 0)
+            {
+                net = originPrice - discNominal;
+            }else if (discNominal > 0 && discPercentage > 0)
+            {
+                net = originPrice - discNominal;
+            }
+            return net;
+        }
+    }
+}
---
+++ CouponWithoutOCP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CoupunWithoutOCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coupon coupon1 = new Coupon();
+            coupon1.discNominal = 2000;
+            Console.WriteLine(coupon1.priceNeet(10000));
+
+            Coupon coupon2 = new Coupon();
+            coupon2.discPercentage = 15;
+            Console.WriteLine(coupon2.priceNeet(10000));
+
+            Coupon coupon3 = new Coupon();
+            coupon3.discPercentage = 90;
+            coupon3.discNominal = 3000;
+            Console.WriteLine(coupon3.priceNeet(10000));
+        }
+    }
+}
